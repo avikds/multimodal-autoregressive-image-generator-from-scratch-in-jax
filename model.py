120b80d1432dde9f2ed2284ff56eeb9421bb0e2e
Avik Das <avikd957@gmail.com>
2026-08-28 13:21:15 +0530

    Step 50: transformer_loss_and_grads

--- model.py
+++ model.py
@@ -647,6 +647,8 @@ def image_token_cross_entropy(logits, target_ids, image_start_index):
     return jnp.mean(losses)
 
 # Step 50 - transformer_loss_and_grads
+jax.config.update("jax_enable_x64", True)
+
 def transformer_loss_and_grads(
     params,
     batch_sequences,
